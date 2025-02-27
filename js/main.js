f87319ea5889c595323027c6387a78c1a0963849
Kvaaak <veikkaknuutila@gmail.com>
2025-02-27 14:14:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import { CSS3DObject } from './libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener("DOMContentLoaded", () => {
